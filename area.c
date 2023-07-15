efc283fc867c2bc854e67166d1e3c271410c5741
azizulhakimrayyan <139643813+azizulhakimrayyan@users.noreply.github.com>
2023-07-15 23:03:56 +0600

    Add files via upload
    area of a circle

---
+++ area.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+//circle area= π r^2
+int main () {
+    float pi=3.1416;
+    float r;
+    printf ("enter r");
+    scanf ("%f",&r);
+
+printf ("the area of a circle is %f" ,pi*(r*r));
+
+
+    return 0;
+}
