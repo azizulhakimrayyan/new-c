2573556f3b0960650449a5a404d137775e944534
azizulhakimrayyan <139643813+azizulhakimrayyan@users.noreply.github.com>
2023-07-15 23:02:52 +0600

    Add files via upload

---
+++ c_to_f.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+// celcius to farenheit 
+int main () {
+float celcius;
+printf("enter celcius");
+scanf("%f",&celcius);
+
+printf("farenheit is %f",(celcius*1.8)+32 );
+
+
+
+
+    return 0;
+}
