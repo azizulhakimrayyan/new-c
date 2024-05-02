ed33c0de2d6196739b8bc8dc9da3dc7883f3e15b
azizulhakimrayyan <139643813+azizulhakimrayyan@users.noreply.github.com>
2024-05-02 20:19:00 +0600

    Update c_to_f.c

--- c_to_f.c
+++ c_to_f.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 int main()
 {
   float c, f;
-  printf("Enter the temperature in celcius:");
+  printf("Enter the temperature in celsius:");
   scanf("%f", &c);
-
   printf("Temperature in farenheit:%f\n", (c * 1.8) + 32);
   printf("Enter the temperature in farenheit:");
   scanf("%f", &f);
-
-  printf("Temperature in celcius:%f", ((f - 32) * 100) / 180);
+  printf("Temperature in celsius:%f", ((f - 32) * 100) / 180);
   return 0;
 }
+
